c65874e683d647359e96b37a88dac7096a71e0b5
Anhelina Sadovnikova <sadovanh@cvut.cz>
2023-02-21 17:06:42 +0100

    hra cv1

---
+++ cv1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+
+Random nh = new Random();
+int cislo = nh.Next(0, 101);
+Console.WriteLine(cislo.ToString());
+
+Console.Write("Zadej cislo: ");
+int cisloUzivatele = Convert.ToInt32(Console.ReadLine());
+
+
+while (true)
+    {
+    if (cislo > cisloUzivatele)
+    {
+            Console.WriteLine("Zadej vetsi cislo");
+            cisloUzivatele = Convert.ToInt32(Console.ReadLine());
+    }
+    else if (cislo < cisloUzivatele)
+    {
+            Console.WriteLine("Zadej mensi cislo");
+            cisloUzivatele = Convert.ToInt32(Console.ReadLine());
+    }
+    else if (cisloUzivatele == cislo)
+    {
+        Console.WriteLine($"Spravne, je to cislo {cislo}");
+    }
+}
